478433c07288ee747118ef9f9b12723bb51fde1a
Konrad Małek <KMALEKSEC@gmail.com>
2016-05-21 00:32:31 +0200

    dodanie funkcji kopiującej tablicę

---
+++ array.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <new>
+
+using namespace std;
+
+void copy(int*** nowa, int** old, int rows, int cols)
+{
+    int i,j;
+    *nowa = new int *[rows];
+
+    if(*nowa == NULL)
+    {
+        cout<<"Blad alokacji tablicy";
+        return;
+    }
+
+    for(i=0;i<rows;i++)
+    {
+        (*nowa)[i] = new int [cols];
+
+        if((*nowa)[i] == NULL)
+        {
+            cout<<"Blad alokacji tablicy";
+            return;
+        }
+    }
+
+    for(i=0;i<rows;i++)
+        for(j=0;j<cols;j++)
+            (*nowa)[i][j]=old[i][j];
+    return;
+}
